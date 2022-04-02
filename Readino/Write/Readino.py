044cd67d4ac21a2c9e68c93e9632dab685e8646c
Patjhoi <102881885+Patjhoi@users.noreply.github.com>
2022-04-02 17:26:39 -0500

    Add files via upload

---
+++ Readino/Write/Readino.py
@@ -0,0 +1,179 @@
+from html.entities import name2codepoint
+from tkinter import X
+from unicodedata import name
+
+
+f = open('Write.ino', 'r')
+pos = (('int', 'float', 'byte', 'bool', 'constante'),
+       '\t\t\t\t\t Se declara la funcion',
+       '\t\t\t\t\t Se declara la funcion en loop en donde irá el programa',
+       '\t Se pone un breve comentario',
+       '\t\t\t\t Se declara el pin',
+       '\t\t\t\t Se lee el pin',
+       '\t\t\t\t\t\t Se cierra el corchete',
+       '\t\t\t\t\t Se pone un delay',
+       '\t\t\t\t\t\t Se realiza un salto de linea')
+state=('como salida', 'como entrada', 'y se activa la resistencia de pull up',
+       'en alto', 'en bajo', 'INDEFINIDO')
+while(True):
+    line=f.readline()
+    if('int' in line):
+        p = line.rstrip('\n')
+        print(p,"\t\t\t\t\t Se declara una variable de tipo", pos[0][0], "con el nombre: ", end="")
+        p = p.replace(" ","")
+        p = p.replace('int', '')
+        name = []
+        for var in p:
+            if (var.isalpha()) | (var == '_'):
+                name.append(var)
+                print(var, end = "")
+            else:
+                print("")
+                break
+        name = str(name)
+        transTable = name.maketrans("", "", "'[], ")
+        name = name.translate(transTable)
+        # print(name)
+        
+        # p = p.replace('=', '')
+        # print(p)
+        # for dig in p:
+        #     print(dig)
+        #     if (dig.isnumeric()):
+        #         print(dig, end=" ") 
+        #     else:
+        #         print("")
+        #         break
+
+    elif('float' in line):
+        p = line.rstrip('\n')
+        print(p,"\t\t\t\t\t Se declara una variable de tipo", pos[0][1], "con el nombre: ", end="")
+        p = p.replace(" ","")
+        p = p.replace('float', '')
+        name = []
+        for var in p:
+            if (var.isalpha()) | (var == '_'):
+                name.append(var)
+                print(var, end = "")
+            else:
+                print("")
+                break
+        name = str(name)
+        transTable = name.maketrans("", "", "'[], ")
+        name = name.translate(transTable)
+        # print(name)
+
+    elif('byte' in line):
+        p = line.rstrip('\n')
+        print(p,"\t\t\t\t\t Se declara una variable de tipo", pos[0][2], "con el nombre: ", end="")
+        p = p.replace(" ","")
+        p = p.replace('byte', '')
+        name = []
+        for var in p:
+            if (var.isalpha()) | (var == '_'):
+                name.append(var)
+                print(var, end = "")
+            else:
+                print("")
+                break
+        name = str(name)
+        transTable = name.maketrans("", "", "'[], ")
+        name = name.translate(transTable)
+        # print(name)
+
+    elif('bool' in line):
+        p = line.rstrip('\n')
+        print(p,"\t\t\t\t\t Se declara una variable de tipo", pos[0][3], "con el nombre: ", end="")
+        p = p.replace(" ","")
+        p = p.replace('bool', '')
+        name = []
+        for var in p:
+            if (var.isalpha()) | (var == '_'):
+                name.append(var)
+                print(var, end = "")
+            else:
+                print("")
+                break
+        name = str(name)
+        transTable = name.maketrans("", "", "'[], ")
+        name = name.translate(transTable)
+        # print(name)
+
+    elif('#DEFINE' in line):
+        p = line.rstrip('\n')
+        print(p,"\t\t\t\t\t Se declara una variable de tipo", pos[0][4], "con el nombre: ", end="")
+        p = p.replace(" ","")
+        p = p.replace('#DEFINE', '')
+        name = []
+        for var in p:
+            if (var.isalpha()) | (var == '_'):
+                name.append(var)
+                print(var, end = "")
+            else:
+                print("")
+                break
+        name = str(name)
+        transTable = name.maketrans("", "", "'[], ")
+        name = name.translate(transTable)
+        # print(name)
+
+
+        
+    elif('void setup' in line):
+        print(line.rstrip('\n'), pos[1])
+    elif('void loop' in line):
+        print(line.rstrip('\n'), pos[2])
+    elif('//' in line):
+        print(line.rstrip('\n'), pos[3])
+    elif('pinMode' in line):
+        p=line.rstrip('\n')
+        print(p, pos[4], end = " ")
+        p=p.replace(' ', '')    
+        print(p[8], end = "")
+        print(p[9].replace(',', ' '), end = " ")
+        s= p.split(sep = ',')
+        if('INPUT' in s[1]):
+            print(state[1], end = " ")
+            if('INPUT_PULLUP' in s[1]):
+                print(state[2])
+            else:
+                print('\n')
+        elif('OUTPUT' in s[1]):
+            print(state[0])
+        else:
+            print(state[5])
+    elif('digitalWrite' in line):
+        p=line.rstrip('\n')
+        print(p, pos[4], end = " ")
+        p=p.replace(' ', '')
+        print(p[13], end = "")
+        print(p[14].replace(',', ' '), end = " ")
+        s= p.split(sep = ',')
+        if('HIGH' in s[1]):
+            print(state[3])
+        elif('LOW' in s[1]):
+            print(state[4])
+        else:
+            print(state[5])
+    # elif('digitalRead' in line):
+    #     p = line.rstrip('\n')
+    #     print(p, pos[4], end=" ")
+    #     p = p.replace(' ', '')
+    #     print(p[13], end="")
+    #     print(p[14].replace(',', ' '), end=" ")
+    #     s = p.split(sep=',')
+    #     if('HIGH' in s[1]):
+    #         print(state[2])
+    #     elif('LOW' in s[1]):
+    #         print(state[3])
+    #     else:
+    #         print(state[4])
+    elif('}' in line):
+        print(line.rstrip('\n'), pos[6])
+    elif('delay' in line):
+        print(line.rstrip('\n'), pos[7])
+    elif('\n' in line):
+        print(line.rstrip('\n'), pos[8])
+    if not line:
+        break
+f.close()
---
+++ Readino/Write/Write.ino
@@ -0,0 +1,23 @@
+int name_var=2;
+float mivariable=3;
+bool stae;
+byte ddk;
+#DEFINE dkdkkd;
+
+digitalWrite(14,HIGH);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,INPUT);
+  pinMode(11,INPUT_PULLUP);
+  pinMode(12,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12,HIGH);
+  delay(100);
+  int hol;
+  digitalWrite(12,LOW);
+}
